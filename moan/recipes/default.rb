916b6af47c3e97591d97a3d4814662449e0bec26
root <root@moan.0.0.1>
2016-01-05 12:00:41 +0000

    nodejs

--- moan/recipes/default.rb
+++ moan/recipes/default.rb
@@ -6,3 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+%w{vim mc lsof nc git telnet unzip curl}.each do |pkg|
+	package pkg do
+	action :upgrade
+	end
+end
+
+%w{apache2 nodejs}.each do |pgk|
+        package pkg do
+        action :upgrade
+        end
+end
+
