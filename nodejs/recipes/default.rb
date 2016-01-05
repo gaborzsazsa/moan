eca49758a65f2ae77eb6c95146b9bd95303ba029
root <root@moan.0.0.1>
2016-01-05 12:44:05 +0000

    nodejs

---
+++ nodejs/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: nodejs
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w{apache2 nodejs}.each do |pgk|
+        package pkg do
+        action :upgrade
+        end
+end
+
+
